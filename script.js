8d2c4421c418075c594e63bcd15afa7172501b5c
Urunesheyi Ejovwo-Ottoh <uruneejovwoottoh@gmail.com>
2023-07-26 17:45:42 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log('Resources');
+console.log(3 * 45);
+console.log(540 % 19);
+console.log('Get ' + 'excited!')
+console.log('For the first time ever!'.length)
+
+var favoriteAnime = 'My Hero Academia'
+var yearIStartedWatching = 2018
+console.log(favoriteAnime)
+console.log(yearIStartedWatching)
+
+let isTegaTheSmartest = false;
+isTegaTheSmartest = true
+console.log(isTegaTheSmartest)
+const myName = 'Urunesheyi'
+console.log(myName)
+console.log('My name is ' + myName)
+console.log(`My name is ${myName}. My favorite anime is ${favoriteAnime}.`)
+
+
+function practiceFunction(u,i) {
+    return u + i;
+}
+
+let mathOperation = practiceFunction(9,2)
+console.log(mathOperation)
